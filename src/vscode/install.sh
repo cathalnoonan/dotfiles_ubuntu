5da823f3409cc3d1f34e78e55461abc1c043521a
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2023-01-25 20:06:39 +0000

    Add "-y" flag to all apt-get install commands (#12)

--- src/vscode/install.sh
+++ src/vscode/install.sh
@@ -10,14 +10,14 @@ if [ "$DOTFILES_IS_DOCKER$DOTFILES_IS_WSL" != "" ]; then
     echo
 else
     # Add repository
-    sudo apt-get install wget gpg
+    sudo apt-get install -y wget gpg
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
     sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
     sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
     rm -f packages.microsoft.gpg
 
     # Update package cache and install
-    sudo apt install apt-transport-https
+    sudo apt install -y apt-transport-https
     sudo apt update
-    sudo apt install code # or code-insiders
+    sudo apt install -y code # or code-insiders
 fi
