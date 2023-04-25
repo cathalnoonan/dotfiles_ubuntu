aed90946eec2a92fbb49477afcbd6cfd2cb11bb3
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2023-04-25 20:51:13 +0100

    Customize prompt

--- src/env.sh
+++ src/env.sh
@@ -29,3 +29,18 @@ if [[ $(grep -i Microsoft /proc/version) ]]; then export DOTFILES_IS_WSL=1; fi;
 #
 export DOTFILES_IS_DOCKER=
 if grep -sq 'docker\|lxc' /proc/1/cgroup; then export DOTFILES_IS_DOCKER=1; fi
+
+##
+# Customize prompt
+##
+color_blue="\[\033[01;34m\]"
+color_green="\[\033[01;32m\]"
+color_red="\[\033[0;91m\]"
+color_yellow="\[\033[0;93m\]"
+color_reset="\[\033[00m\]"
+parse_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+export PS1="
+${debian_chroot:+($debian_chroot)}${color_green}\u@\h${color_reset}:${color_blue}\w${color_yellow}\$(parse_git_branch)$color_reset
+$ "
