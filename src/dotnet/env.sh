dd3722a8e1a9012f8a0072321cb288958b9037fc
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2023-05-13 15:21:49 +0100

    Include dotnet tools folder in PATH

--- src/dotnet/env.sh
+++ src/dotnet/env.sh
@@ -3,3 +3,10 @@
 export DOTNET_CLI_TELEMETRY_OPTOUT=true
 export DOTNET_NOLOGO=true
 export DOTNET_NUGET_SIGNATURE_VERIFICATION=false
+
+##
+# Include dotnet tools folder in PATH
+##
+if [[ "$PATH" != *"/.dotnet/tools"* ]]; then
+    export PATH="$PATH:$HOME/.dotnet/tools"
+fi
