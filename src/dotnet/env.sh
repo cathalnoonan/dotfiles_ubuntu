24d7f0e1d7d0f6b3d1ff2d0ac0492d989df8927a
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2023-05-22 20:29:33 +0100

    Include DOTNET_ROOT in PATH

--- src/dotnet/env.sh
+++ src/dotnet/env.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+export DOTNET_ROOT=$HOME/.dotnet
 export DOTNET_CLI_TELEMETRY_OPTOUT=true
 export DOTNET_NOLOGO=true
 export DOTNET_NUGET_SIGNATURE_VERIFICATION=false
 
 ##
+# Include dotnet in PATH
+##
+if [[ "$PATH" != *"$DOTNET_ROOT"* ]]; then
+    export PATH="$PATH:$DOTNET_ROOT"
+fi
+
+##
 # Include dotnet tools folder in PATH
 ##
-if [[ "$PATH" != *"/.dotnet/tools"* ]]; then
-    export PATH="$PATH:$HOME/.dotnet/tools"
+if [[ "$PATH" != *"$DOTNET_ROOT/tools"* ]]; then
+    export PATH="$PATH:$DOTNET_ROOT/tools"
 fi
